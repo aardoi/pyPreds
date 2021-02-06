8925a7bc9bec65906b970c78948f81416d96c3fb
unknown <aubin.ardois@gmail.com>
2021-02-06 16:05:16 +0100

    adding data_gathering.py file

---
+++ src/data_gathering.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+
+# File : data_gathering.py of the Project "src" Created by Aubin ARDOIS at 16:03 the 06/02/2021 
+# with PyCharm 
+
+"""
+________________________________________________________________________________________________________________________
+
+_____________________________________________________-HEADER-___________________________________________________________
+
+
+script name : data_gathering.py
+------------
+
+content : This file countains
+--------
+
+purpose : This (class or script or function) purpose is to ...
+________________________________________________________________________________________________________________________
+"""
+
+#  *****************
+# | PACKAGES IMPORT |
+#  *****************
+
+#  ************************
+# | AUTHORSHIP INFORMATION |
+#  ************************
+
+__author__ = "aardoi"
+__copyright__ = "
+__credits__ = ["aardoi"]
+__license__ = ""
+__version__ = "0.0.1
+__maintainer__ = "aardoi"
+__email__ = ""
+__status__ = ["Development"]
+
+#  **************************************
+# | CLASS / FUNCTION / SCRIPT DEFINITION |
+#  **************************************
+
